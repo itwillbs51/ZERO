8a8a9e3e683bdd394dc1eac96204c3ec352b6ef2
sonJY <user@ITWILL>
2023-07-28 09:03:21 +0900

    손지영 - 230728 - 임시

--- ZERO/src/main/java/com/itwillbs/zero/controller/ChatController.java
+++ ZERO/src/main/java/com/itwillbs/zero/controller/ChatController.java
@@ -1,39 +1,38 @@
-package com.itwillbs.zero.controller;
-
-import java.util.*;
-
-import javax.servlet.http.*;
-
-import org.springframework.security.core.context.*;
-import org.springframework.security.core.userdetails.*;
-import org.springframework.stereotype.*;
-import org.springframework.ui.*;
-import org.springframework.web.bind.annotation.*;
-
-@Controller
-public class ChatController {
-	
-	
-	
-	// 채팅 페이지로 이동
-	@GetMapping("chat")
-	public String chat(HttpSession session, Model model) {
-		
-		// 임의로 세션을 주어서 채팅 기능 확인해보기
-		int testId = (int)((Math.random()* (9999 - 1000 + 1)) + 1000);
-		session.setAttribute("sId", testId);
-//		System.out.println(testId);
-		// 사용자 정보 출력(세션)
-		/*
-		 */
-		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		System.out.println("user name : " + user.getUsername());
-		System.out.println("normal chat page");
-		
-		model.addAttribute("userId", user);
-		
-		
-		return "chatting/chat";
-	}
-	
-}
+//package com.itwillbs.zero.controller;
+//
+//import java.util.*;
+//
+//import javax.servlet.http.*;
+//
+//import org.springframework.beans.factory.annotation.*;
+//import org.springframework.security.core.context.*;
+//import org.springframework.security.core.userdetails.*;
+//import org.springframework.stereotype.*;
+//import org.springframework.ui.*;
+//import org.springframework.web.bind.annotation.*;
+//
+//import com.itwillbs.zero.service.*;
+//import com.itwillbs.zero.vo.*;
+//
+//import lombok.*;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/chatMsg")
+//public class ChatController {
+//	
+//	@Autowired
+//	private final ChatService chatService;
+//	
+//	@PostMapping
+//	public ChatRoomVO createRoom(@RequestBody String buyer_id) {
+//		return chatService.createRoom(buyer_id);
+//	}
+//	
+//	@GetMapping
+//	public List<ChatRoomVO> findAllRoom() {
+//		return chatService.findAllRoom();
+//	}
+//		
+//	
+//}
