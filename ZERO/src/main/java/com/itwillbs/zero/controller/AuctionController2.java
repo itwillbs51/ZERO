88150ff694a530ef1f9a74f05e084fe971486e3f
user <user@ITWILL>
2023-07-31 15:15:35 +0900

    경매등록

--- ZERO/src/main/java/com/itwillbs/zero/controller/AuctionController2.java
+++ ZERO/src/main/java/com/itwillbs/zero/controller/AuctionController2.java
@@ -47,7 +47,13 @@ public class AuctionController2 {
 
 	// 경매 상품 등록폼
 	@GetMapping("auction_regist_form")
-	public String auction_regist_form(Model model) {
+	public String auction_regist_form(Model model, HttpSession session) {
+		
+		String member_id = (String) session.getAttribute("member_id");
+		if(member_id == null) {
+			model.addAttribute("msg", "로그인이 필요한 작업입니다!");
+			return "fail_back";
+		}
 		List<HashMap<String, String>> category=service.getCategory();
 		List<HashMap<String, String>> brand=service.getBrand();
 		
@@ -60,11 +66,13 @@ public class AuctionController2 {
 	@ResponseBody
 	@PostMapping ("auction_regist_pro")
 	public String auction_regist_pro(AuctionProductVO auctionProduct,HttpSession session, Model model) {
-		
-//		auctionProduct.setAuction_seller_id((String)session.getAttribute("member_id"));
-		auctionProduct.setAuction_seller_id("admin@gmail.com");//임시
-		
-		
+		String member_id = (String) session.getAttribute("member_id");
+		if(member_id == null) {
+			model.addAttribute("msg", "세션이 만료되었습니다 다시 로그인하세요");
+			return "fail_back";
+		}
+		auctionProduct.setAuction_seller_id(member_id);
+				
 		String uploadDir = "/resources/upload"; 
 //		String saveDir = request.getServletContext().getRealPath(uploadDir); // 사용 가능
 		String saveDir = session.getServletContext().getRealPath(uploadDir);
