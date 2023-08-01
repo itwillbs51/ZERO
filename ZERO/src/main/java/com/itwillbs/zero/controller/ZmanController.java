df865bf03b57b050b1b586f49aae35f125be148b
user <user@ITWILL>
2023-08-01 12:33:53 +0900

    230801 - 조윤솔 - ZMAN 지도 구현 테스트 중

--- ZERO/src/main/java/com/itwillbs/zero/controller/ZmanController.java
+++ ZERO/src/main/java/com/itwillbs/zero/controller/ZmanController.java
@@ -1,6 +1,7 @@
 package com.itwillbs.zero.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -34,5 +35,15 @@ public class ZmanController {
 	@GetMapping("zman_myPage")
 	public String zmanMyPage() {
 		return "zman/zman_myPage";
+	}
+	
+	// ZMAN 지도 test 페이지로 이동하기
+	@GetMapping("zman_test_location")
+	public String zman_test_location(Model model) {
+		
+		model.addAttribute("depart", " 부산광역시 부산진구 중앙대로 658"); // zman_delivery_startspot 
+		model.addAttribute("arrive", "부산광역시 부산진구 범전로5번길 13"); // zman_delivery_endspot
+		
+		return "zman/zman_test_location";
 	}
 }
